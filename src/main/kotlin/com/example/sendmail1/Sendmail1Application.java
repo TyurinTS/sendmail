18ae9c65cbaae5d6eba94b02c500defcf4ad3d7c
Tyurin-TS <timt@mail.ru>
2021-05-27 08:52:02 +0300

    add production profile

---
+++ src/main/kotlin/com/example/sendmail1/Sendmail1Application.java
@@ -0,0 +1,11 @@
+package com.example.sendmail1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sendmail1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sendmail1Application.class);
+    }
+}
